66795df9858b840c1a6fc1f78a4970d4eda1af24
Ivan <ivancabeza85@gmail.com>
2020-05-15 20:45:07 +0200

    Sección 4

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import Server from "./classes/server";
+import Server from './classes/server';
 import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
 
-const server = new Server();
+const server = Server.instance;
 
 
 // BodyParser
